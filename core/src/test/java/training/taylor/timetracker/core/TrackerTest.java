bcf6d9b5e2fb78375c887b8611c5b5c48529f3bb
Kunal Kumar <103275977+kunalrajshah@users.noreply.github.com>
2025-09-13 14:28:01 +0530

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -1,7 +1,9 @@
 package training.taylor.timetracker.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,27 +11,31 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
-/**
- * Created by Jason on 6/19/2015.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TrackerCoreConfig.class)
 public class TrackerTest {
+
     @Autowired
     private Tracker tracker;
 
-    @Test
-    public void testMe() {
-        assertNotNull(tracker);
+    @Before
+    public void setUp() {
+        // Ensure tracker starts clean before each test
+        while (tracker.size() > 0) {
+            tracker.remove(tracker.get(0));
+        }
     }
 
     @Test
-    public void testAdd() {
-        TimeEntry entry = new TimeEntry();
-        entry.setDescription("Entry Test");
-        entry.setRate(80.0f);
-        entry.setTime(3);
+    public void testTrackerBeanInjection() {
+        assertNotNull("Tracker bean should be injected by Spring", tracker);
+    }
+
+    @Test
+    public void testAddEntry() {
+        TimeEntry entry = new TimeEntry("Entry Test", 80.0f, 3);
         tracker.add(entry);
-        assertTrue(tracker.size() > 0);
+        assertEquals("Tracker should contain exactly 1 entry", 1, tracker.size());
+        assertEquals("Stored entry should match the added entry", entry, tracker.get(0));
     }
 }
