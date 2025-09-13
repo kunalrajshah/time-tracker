2686b7507aabd90b3e7bd7c4a2c5e8686bc60059
Kunal Kumar <103275977+kunalrajshah@users.noreply.github.com>
2025-09-13 14:25:24 +0530

    Update Tracker.java new changes

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,25 +1,26 @@
 package training.taylor.timetracker.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class Tracker {
-    @Autowired
-    private List<TimeEntry> entries;
+
+    private final List<TimeEntry> entries = new ArrayList<>();
 
     public void add(TimeEntry entry) {
-        entries.add(entry);
+        if (entry != null) {
+            entries.add(entry);
+        }
     }
 
     public void remove(TimeEntry entry) {
-        if (true)
+        if (entry != null) {
             entries.remove(entry);
-
-        entries.remove(entry);
+        }
     }
 
     public int size() {
@@ -27,18 +28,9 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
+        if (index >= 0 && index < entries.size()) {
+            return entries.get(index);
         }
-
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
-        return entries.get(index);
+        throw new IndexOutOfBoundsException("Invalid index: " + index);
     }
 }
